a4f9a5807eab0481c5a9574df2fd53c9af3ba548
theGeekkit <sonofgondolin16@gmail.com>
2023-03-30 17:15:48 -0500

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './header/header.component';
+import { DeckComponent } from './deck/deck.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DeckComponent
   ],
   imports: [
     BrowserModule,
